3db35c5bcdf27b5526523882b9cceba533128bb2
liubinbin <lotosbin@gmail.com>
2016-07-23 02:06:00 +0800

    first

---
+++ src/Host/Startup.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using Hangfire.Redis;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(options=>{});
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddLogging();
+
+            // Add our repository type
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseStorage(new RedisStorage("127.0.0.1:6379"));
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
